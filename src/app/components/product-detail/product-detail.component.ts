285f30be02bd1c5af86d0f5d75ec008e8a8f1920
Edinson Colonia <EdinsonColonia@gmail.com>
2022-01-23 20:09:36 -0500

    Detalle del producto en product-detail. Estilizado y trabajado con flexbox

--- src/app/components/product-detail/product-detail.component.ts
+++ src/app/components/product-detail/product-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 import { switchMap } from 'rxjs/operators';
 import { Product } from '../../models/product.model'
 import { StoreService } from './../../services/store.service'
@@ -10,33 +10,20 @@ import { StoreService } from './../../services/store.service'
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.scss']
 })
-export class ProductDetailComponent implements OnInit{
+export class ProductDetailComponent implements OnInit {
 
-    productId: string | null = null;
-  product: Product | null = null;
-
+  product!: Product;
+  productId: string | null = null;
+  
     constructor(
     private route: ActivatedRoute,
     private storeService: StoreService
-
   ) { }
 
   ngOnInit(): void {
-    this.route.paramMap
-    .pipe(
-      switchMap((params) => {
-        this.productId = params.get('id');
-        if (this.productId) {
-         // return this.storeService.getShoppingCart(this.productId);
-        }
-        return [null];
-      })
-    )
-    // .subscribe((data) => {
-    //  this.product = data;
-    //});
+    this.route.params.subscribe((params: Params) => {
+      const id = params.id;
+      this.product = this.storeService.getProduct(id)!;
+    });  
   }
-
-  
-
 }
--- src/app/services/store.service.ts
+++ src/app/services/store.service.ts
@@ -1,12 +1,45 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
-
 import { Product } from '../models/product.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StoreService {
+
+  // prueba de array
+
+  products: Product[] = [
+    {
+      id: '1',
+      name: 'Pan con aceitunas del Valle frescas y jugosas  (pack * 4)',
+      price: 12900,
+      image: './assets/images/plate1.png',
+      title: 'Pan Bettel Aceitunas'
+    },
+    {
+      id: '2',
+      name: 'Arandanos tiernos con la mezcla perfecta del pan Bettel (pack * 4)',
+      price: 12900,
+      image: './assets/images/plate2.png',
+      title: 'Pan Bettel Arandanos'
+    },
+    {
+      id: '3',
+      name: 'La combinacion del Cacao del Pacifico y un Pan fresco y crujiente (pack * 4)',
+      price: 12900,
+      image: './assets/images/plate3.png',
+      title: 'Pan Bettel Cacao'
+    },
+    {
+      id: '4',
+      name: 'Pan relleno hasta las puntas de queso fresco y suave (pack * 4)',
+      price: 12900,
+      image: './assets/images/plate4.png',
+      title: 'Pan Bettel Queso'
+    },
+  ];
+
 
   private myShoppingCart: Product[] = [];
   private myCart = new BehaviorSubject<Product[]>([]);
@@ -15,6 +48,11 @@ export class StoreService {
 
   constructor() { }
 
+ 
+  getProduct(id: string) {
+    return this.products.find(item => id === item.id);
+  }
+ 
   addProduct(product: Product) {
     this.myShoppingCart.push(product);
     this.myCart.next(this.myShoppingCart);
